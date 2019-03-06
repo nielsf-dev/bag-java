4b8c405bd4feb5f999cea642e0f0d99447e7b4ca
niels <niels@bakkerspees.nl>
2019-03-06 12:10:31 +0100

    als dit maar goed gaat

---
+++ updater/src/main/java/updaterpck/UpdateApp.java
@@ -0,0 +1,16 @@
+package updaterpck;
+
+import org.bag.domain.Image;
+import org.bag.domain.Project;
+
+import java.util.ArrayList;
+
+public class UpdateApp
+{
+    public static void main(String[] args) throws Exception {
+        ArrayList<Image> images = new ArrayList<>();
+        images.add(new Image("test.png"));
+        Project p = new Project("test","locatie", images,0,0);
+        System.out.println(p.title());
+    }
+}
