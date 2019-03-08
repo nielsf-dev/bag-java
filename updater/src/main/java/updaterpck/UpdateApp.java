9a2772aed9d9269195c6dc2f72a8344f121f1286
Niels <niels@bakkerspees.nl>
2019-03-08 21:28:17 +0100

    jfd mainframe

--- updater/src/main/java/updaterpck/UpdateApp.java
+++ updater/src/main/java/updaterpck/UpdateApp.java
@@ -2,7 +2,8 @@ package updaterpck;
 
 import org.bag.domain.Image;
 import org.bag.domain.Project;
-
+import com.bulenkov.darcula.DarculaLaf;
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class UpdateApp
@@ -12,5 +13,10 @@ public class UpdateApp
         images.add(new Image("test.png"));
         Project p = new Project("test","locatie","", images,0,0);
         System.out.println(p.getTitle("nl"));
+
+        UIManager.setLookAndFeel(new DarculaLaf());
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
     }
 }
