93d177a153afcb75b2948b49337b4e957891ae19
niels <niels@bakkerspees.nl>
2019-03-21 11:00:59 +0100

    login gedeelte werkende

---
+++ login.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -i -X POST -d username=user -d password=userPass -c ./bag-cookie.txt http://localhost:5000/login 
+
+curl -i --header "Accept:application/json" -X GET -b ./bag-cookie.txt http://localhost:5000/secret/project
